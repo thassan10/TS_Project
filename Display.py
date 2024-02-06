69f97c0aa9ad83403ad1c499a93c622928d63a6c
Tasmina Hassan <tasmina.hassan10@myhunter.cuny.edu>
2024-02-06 12:55:43 -0500

    pillow

--- Display.py
+++ Display.py
@@ -7,39 +7,3 @@ img = Image.open("geek.jpg")
 img.thumbnail((400,400))
 img.save('new_img.jpg')
 #img.show()
-
-# Can use simpy to move to another window but then have to use opencv to get a np array as input
-
-# CAN'T IMPORT CV2 FOR SOME REASON
-#import os
-#import time
-#from multiprocessing import Process
-#import cv2
-
-#def f(display):
-#    os.environ['DISPLAY'] = display
-#    print(os.environ['DISPLAY'])
-#    a = cv2.imread('avatar.png')
-#    cv2.imshow('window on %s'%display, a)
-#    cv2.waitKey(1000)
-#    time.sleep(10)
-
-#Process(target=f, args=(':0.0',)).start()
-#Process(target=f, args=(':0.1',)).start()
-
-# slmpy doesn't work on Mac lol
-# Think this is most promising - want to test on lab comp
-import slmpy
-import numpy as np
-import time
-
-slm = slmpy.SLMdisplay()
-# slm = slmpy.SLMdisplay(monitor = x) - To specify monitor with x being monitor
-slm = slmpy.SLMdisplay(isImageLock = False)
-resX, resY = slm.getSize()
-testIMG = np.zeros([resY,resX]).astype('uint8')
-t0 = time.time()
-for i in range(100):
-  slm.updateArray(testIMG)
-print(time.time() - t0)
-slm.close()
